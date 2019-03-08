8d887cfb455d63fa87b0c0fba0eb3b686ad8ea7c
taraldjoh <41441415+taraldjoh@users.noreply.github.com>
2019-03-08 20:38:46 +0100

    store task in localstorage

--- app.js
+++ app.js
@@ -44,10 +44,28 @@ function addTask(e) {
   // Append li to ul
   taskList.appendChild(li);
 
+
+  // Store in LS
+  storeTaskInLocalStorage(taskInput.value);
+
   // Clear input
   taskInput.value = "";
 
   e.preventDefault();
+}
+
+// Store task
+function storeTaskInLocalStorage(task){
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.push(task);
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 // Remove TAsk
